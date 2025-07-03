31eb8bf89ea1fa8228899ab5115774c4d17360af
EnriqueCarrillo1110 <excqui311@gmail.com>
2025-07-03 13:23:01 -0600

    feat[components]: agregar nuevos componentes principales del sistema

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>© 2025 Mi Proyecto - Todos los derechos reservados</p>
-    </footer>
-  );
-}
-export default Footer;
--- src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <main>
-      <h2>Bienvenido a la página principal</h2>
-      <p>Esta es la sección de inicio de nuestro sitio.</p>
-    </main>
-  );
-}
-export default Home;
